87de260a71d44f3f64e885a1a41331e23e3d3f6f
dshearwood <d.algorhythmic@gmail.com>
2021-07-13 09:45:33 -0400

    Added color and placement  to the Navbar

--- movie-list/src/Navbar.js
+++ movie-list/src/Navbar.js
@@ -4,20 +4,20 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div>
-      <div class="nav">What to watch</div>
+      <div class="nav"><strong>What to watch</strong>
       <form action="">
         <div class="inputFields">
           <input type="search" placeholder="Title" />
           <label for="" class=""></label>
-          <select name="Type" id="" class="type">Type
-              <option value="Type"></option>
+          <select name="Type" id="Type" class="type">
               <option value="movies">Movies</option>
               <option value="series">Series</option>
               <option value="episodes">Episodes</option>
           Type</select>
-          <button></button>
+          <button>Search</button>
         </div>
       </form>
+      </div>
     </div>
   );
 }
